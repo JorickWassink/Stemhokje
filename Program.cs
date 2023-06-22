49347df444d818664e7a30e876e238d90d6b5dc6
Jorick Wassink <129957118+JorickWassink@users.noreply.github.com>
2023-06-22 08:29:56 +0100

    Finish
    Project was already finished

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿namespace stemhokje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool heelgoedeloop = true;
+            int dominique = 0;
+            int zacharia = 0;
+            while (heelgoedeloop)
+            {
+                Console.WriteLine("president Dominique of president Zacharia?");
+                string stem = Console.ReadLine();
+                string stemlower = stem.ToLower();
+                if (stemlower.Contains("dominique"))
+                {
+                    Console.WriteLine("je hebt gestemd");
+                    dominique++;
+                }
+                else if (stem.Contains("zacharia"))
+                {
+                    Console.WriteLine("je hebt gestemd");
+                    zacharia++;
+                }
+                else if (stem.Contains("uitslag"))
+                {
+                    Console.WriteLine($"die uh dominogwattes heeft {dominique} aantal stemmen");
+                    Console.WriteLine($"die andere heeft {zacharia} aantal stemmen");
+                    if (dominique > zacharia)
+                    {
+                        Console.WriteLine("wow dominogwattes wint");
+                    }
+                    else if (zacharia > dominique)
+                    {
+                        Console.WriteLine("wow zacharia wint");
+                    }
+                    else if (zacharia == dominique)
+                    {
+                        Console.WriteLine("hoe is het gelijkspel geworden wat");
+                    }
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
